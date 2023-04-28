ad6d3e6d28b1ef2ca2d933bf35287da5a66e3061
Vadym <laurencekiev@gmail.com>
2023-04-28 22:32:35 +0300

    Refactor entity Product

--- src/main/java/com/kopylov/onlineshop/entity/Product.java
+++ src/main/java/com/kopylov/onlineshop/entity/Product.java
@@ -12,12 +12,6 @@ public class Product {
         this.name = name;
         this.price = price;
     }
-    //for Tests
-    public Product(String name, double price,LocalDateTime creationDate) {
-        this.name = name;
-        this.price = price;
-        this.creationDate = creationDate;
-    }
 
     public Product(int id, String name, double price) {
         this.id = id;
